15c57c7cd0c105434d4dadecec3b9c80b947dd7f
ika1155 <ishidas1155@gmail.com>
2023-02-25 15:27:19 +0900

    メニュー追加

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,9 +11,12 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+				<div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('flashcard.index')" :active="request()->routeIs('flashcard.index')">
+                        HOME
+                    </x-nav-link>
+                    <x-nav-link :href="route('flashcard.create')" :active="request()->routeIs('flashcard.create')">
+                        新規作成
                     </x-nav-link>
                 </div>
             </div>
@@ -67,8 +70,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('flashcard.index')" :active="request()->routeIs('flashcard.index')">
+                HOME
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('flashcard.create')" :active="request()->routeIs('flashcard.create')">
+                新規作成
             </x-responsive-nav-link>
         </div>
 
